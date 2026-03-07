87f562b0e42598cf04eced61ad6e74e0bbc01264
Oak kub <64689412+oak-kittanai@users.noreply.github.com>
2026-03-07 15:04:29 +0700

    ลืมติก

--- Assets/Scripts/LevelScript/LevelData.cs
+++ Assets/Scripts/LevelScript/LevelData.cs
@@ -18,6 +18,7 @@ public class LevelData : MonoBehaviour
 
     [Header("Interact Objects")]
     public GameObject[] holdStepButtons;
+    public GameObject[] toggleStepButtons;
     public GameObject[] rockTriggerDoors;
 
     [Header("Traps")]
