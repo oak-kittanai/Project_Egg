f13de2b44f7aa9b6d3e1ae7e833cde7183381e20
Oak kub <64689412+oak-kittanai@users.noreply.github.com>
2026-04-02 11:57:26 +0700

    update Tutorials

--- Assets/Scripts/LevelScript/Tutorial/TutorialTrigger.cs
+++ Assets/Scripts/LevelScript/Tutorial/TutorialTrigger.cs
@@ -1,43 +1,100 @@
 using UnityEngine;
+using System.Collections.Generic;
 
+[RequireComponent(typeof(Collider2D))]
 public class TutorialTrigger : MonoBehaviour
 {
+    public enum TutorialTarget { Both, Bird, Duck }
+
     [Header("Trigger Setup")]
     public string targetTutorialName;
-
+    public TutorialTarget showFor = TutorialTarget.Both;
+    public bool hideOnExit = false;
     public bool showOnlyOnce = true;
+
     private bool hasShown = false;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (showOnlyOnce && hasShown) return;
+    private bool isPlayerCurrentlyInside = false;
 
-        if (other.CompareTag("Player"))
+    private Collider2D triggerCollider;
+    private ContactFilter2D contactFilter;
+    private List<Collider2D> overlappedColliders = new List<Collider2D>();
+
+    private void Start()
+    {
+        triggerCollider = GetComponent<Collider2D>();
+        triggerCollider.isTrigger = true;
+
+        contactFilter = new ContactFilter2D();
+        contactFilter.NoFilter();
+    }
+
+    private void Update()
+    {
+        if (showOnlyOnce && hasShown && !isPlayerCurrentlyInside) return;
+
+        int colliderCount = triggerCollider.OverlapCollider(contactFilter, overlappedColliders);
+
+        bool foundLocalPlayer = false;
+        MovementCharacter localPlayerMC = null;
+
+        for (int i = 0; i < colliderCount; i++)
         {
-            var mc = other.GetComponent<MovementCharacter>();
-            if (mc != null && mc.HasInputAuthority)
+            Collider2D other = overlappedColliders[i];
+            if (other.CompareTag("Player"))
             {
-                if (LevelData.Instance != null)
+
+                var allMCs = other.GetComponents<MovementCharacter>();
+
+                foreach (var mc in allMCs)
                 {
-                    LevelData.Instance.RequestTutorialShow(targetTutorialName);
-                    hasShown = true;
+                    if (mc != null && mc.enabled && mc.HasInputAuthority)
+                    {
+                        foundLocalPlayer = true;
+                        localPlayerMC = mc;
+                        break;
+                    }
                 }
             }
+
+            if (foundLocalPlayer) break;
+        }
+
+        if (foundLocalPlayer && !isPlayerCurrentlyInside)
+        {
+            isPlayerCurrentlyInside = true;
+            HandlePlayerEnter(localPlayerMC);
+        }
+        else if (!foundLocalPlayer && isPlayerCurrentlyInside)
+        {
+            isPlayerCurrentlyInside = false;
+            HandlePlayerExit();
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void HandlePlayerEnter(MovementCharacter mc)
     {
-        if (other.CompareTag("Player"))
+        if (showOnlyOnce && hasShown) return;
+
+        bool isBird = (mc is Bird_Moveset);
+        bool isDuck = (mc is Duck_Moveset);
+
+        bool canShow = (showFor == TutorialTarget.Both) ||
+                       (showFor == TutorialTarget.Bird && isBird) ||
+                       (showFor == TutorialTarget.Duck && isDuck);
+
+        if (canShow && LevelData.Instance != null)
         {
-            var mc = other.GetComponent<MovementCharacter>();
-            if (mc != null && mc.HasInputAuthority)
-            {
-                if (LevelData.Instance != null)
-                {
-                    LevelData.Instance.RequestTutorialHide();
-                }
-            }
+            LevelData.Instance.RequestTutorialShow(targetTutorialName);
+            hasShown = true;
+        }
+    }
+
+    private void HandlePlayerExit()
+    {
+        if (hideOnExit && LevelData.Instance != null)
+        {
+            LevelData.Instance.RequestTutorialHide();
         }
     }
 }
