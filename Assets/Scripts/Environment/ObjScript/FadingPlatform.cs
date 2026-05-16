7459ad255525a7df84b507474d1381f9cfd383bc
PongsathornXCody <pongsathorn.T0511@gmail.com>
2026-05-16 19:52:52 +0700

    Trap Update

--- Assets/Scripts/Environment/ObjScript/FadingPlatform.cs
+++ Assets/Scripts/Environment/ObjScript/FadingPlatform.cs
@@ -1,53 +1,36 @@
 using UnityEngine;
 using Fusion;
-
-[RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(BoxCollider2D))]
 public class FadingPlatform : NetworkBehaviour
 {
     [Header("Fading Setting")]
+    [Tooltip("ความเร็วจาง")]
     [SerializeField] float fadeSpeed = 2.0f;
     [Range(0f, 1f)]
-    [SerializeField] float minOpacity = 0.1f;
-
-    [Header("Respawn Setting")]
-    [SerializeField] float respawnDelay = 2.0f; //หน่วงเวลาเกิด
+    [SerializeField] float minOpacity = 0f; 
 
     private SpriteRenderer sr;
-    private BoxCollider2D col;
 
     //=== ตัวแปร Network ===
     [Networked] private int PlayersCount { get; set; }
-    [Networked] private NetworkBool IsBroken { get; set; }
     [Networked] private float TargetAlpha { get; set; }
-    [Networked] private TickTimer RespawnTimer { get; set; }
 
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
-        col = GetComponent<BoxCollider2D>();
     }
 
     public override void Spawned()
     {
-        TargetAlpha = 1.0f;
+        if (HasStateAuthority)
+        {
+            TargetAlpha = 1.0f;
+            PlayersCount = 0;
+        }
     }
 
     public override void FixedUpdateNetwork()
     {
         if (!HasStateAuthority) return;
-
-        if (IsBroken)
-        {
-            if (RespawnTimer.Expired(Runner))
-            {
-                TargetAlpha = 1.0f;
-                IsBroken = false;
-                RespawnTimer = TickTimer.None;
-            }
-            return;
-        }
-
         TargetAlpha = (PlayersCount > 0) ? minOpacity : 1.0f;
     }
 
@@ -59,21 +42,6 @@ public class FadingPlatform : NetworkBehaviour
             currentColor.a = Mathf.MoveTowards(currentColor.a, TargetAlpha, fadeSpeed * Time.deltaTime);
             sr.color = currentColor;
         }
-
-        if (HasStateAuthority && !IsBroken && PlayersCount > 0 && Mathf.Approximately(sr.color.a, minOpacity))
-        {
-            BreakPlatform();
-        }
-
-        col.enabled = !IsBroken;
-    }
-
-    private void BreakPlatform()
-    {
-        IsBroken = true;
-        PlayersCount = 0;
-        TargetAlpha = 0.0f;
-        RespawnTimer = TickTimer.CreateFromSeconds(Runner, respawnDelay);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -93,7 +61,7 @@ public class FadingPlatform : NetworkBehaviour
     {
         if (!HasStateAuthority) return;
 
-        if (collision.gameObject.CompareTag("Player") && !IsBroken)
+        if (collision.gameObject.CompareTag("Player"))
         {
             PlayersCount = Mathf.Max(0, PlayersCount - 1);
         }
