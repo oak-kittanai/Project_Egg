81aec59b21da58c304a8c5a2f4f0cae297c946c5
Oak kub <64689412+oak-kittanai@users.noreply.github.com>
2026-06-03 20:15:36 +0700

    update

--- Assets/Scripts/Character/MovementCharacter.cs
+++ Assets/Scripts/Character/MovementCharacter.cs
@@ -135,12 +135,9 @@ public class MovementCharacter : NetworkBehaviour, IDamageable
     {
         string itemName = HeldItemName.ToString();
 
-        if (HasInputAuthority && PlayerInterface.Instance != null)
+        if (HasInputAuthority && PlayerInterface.Instance != null && GameManager.Instance != null && GameManager.Instance.IsGameReady)
         {
-            if (string.IsNullOrEmpty(itemName))
-                PlayerInterface.Instance.HideItemOverlay();
-            else
-                PlayerInterface.Instance.ShowItemOverlay(itemName);
+            if (string.IsNullOrEmpty(itemName)) PlayerInterface.Instance.ShowItemOverlay(itemName);
         }
 
         _canThrowItem = (itemName == "Rock");
