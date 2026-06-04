dd7f8b66f8fada6d04876dd52ef2b882eed3962f
Oak kub <64689412+oak-kittanai@users.noreply.github.com>
2026-06-04 15:05:49 +0700

    Revert "update"
    This reverts commit 81aec59b21da58c304a8c5a2f4f0cae297c946c5.

--- Assets/Scripts/Character/MovementCharacter.cs
+++ Assets/Scripts/Character/MovementCharacter.cs
@@ -135,9 +135,12 @@ public class MovementCharacter : NetworkBehaviour, IDamageable
     {
         string itemName = HeldItemName.ToString();
 
-        if (HasInputAuthority && PlayerInterface.Instance != null && GameManager.Instance != null && GameManager.Instance.IsGameReady)
+        if (HasInputAuthority && PlayerInterface.Instance != null)
         {
-            if (string.IsNullOrEmpty(itemName)) PlayerInterface.Instance.ShowItemOverlay(itemName);
+            if (string.IsNullOrEmpty(itemName))
+                PlayerInterface.Instance.HideItemOverlay();
+            else
+                PlayerInterface.Instance.ShowItemOverlay(itemName);
         }
 
         _canThrowItem = (itemName == "Rock");
